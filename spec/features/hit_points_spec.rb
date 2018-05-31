1079f1fdda374c816fe9ac549b09745da9c8411f
Nick <nicholasjohnrichardson@gmail.com>
2018-05-31 22:04:19 +0100

    User Story 6
    As Player 1,
    So I can see how close I am to losing,
    I want to see my own hit points

--- spec/features/hit_points_spec.rb
+++ spec/features/hit_points_spec.rb
@@ -3,4 +3,8 @@ feature 'Players can view hitpoints' do
     sign_in_and_play
     expect(page).to have_content 'Maud: 60HP'
   end
+  scenario 'Player 1 Hit Points can be seen by Player 2' do
+    sign_in_and_play
+    expect(page).to have_content 'John: 60HP'
+  end
 end
